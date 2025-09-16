76d4eaeca6fa519c5223fcf96d1a8c64c96a8685
NeetGupta <neetgupta702@gmail.com>
2025-09-17 05:01:31 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,13 @@
+output azurerm_resource_group_nmae {
+value = azurerm_resource_group.example.name
+}
+output azurerm_resource_group_location {
+value = azurerm_resource_group.example.location
+}
+output vnetname
+{
+value = azurerm_virtual_network.name
+}
+output storage_name {
+value = azurerm_storage_account.name
+}
