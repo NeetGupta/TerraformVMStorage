9cd6cae1840d7c829651f294bcd8835fdc94cbf1
NeetGupta <neetgupta702@gmail.com>
2025-09-17 04:36:15 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable resourcegroup_name {
+   description = "this is resource group name"
+    type = string
+    default = "Terraform"
+}
+variable resourcegroup_location {
+   description = "this is resource group location"
+  type = string
+  default = "West Europe"
+}
+variable vnet_name {
+   description = "this is azure virtual network"
+  type = string
+  default = "azurevnet"
+}
+variable subnet_name {
+   description = "this is azure virtual network"
+  type = string
+  default = "azuresubnetnet"
+}
+variable storage_name {
+   description = "this is azure virtual network"
+  type = string
+  default = "targetstorage"
+}
+  
+  
+  
